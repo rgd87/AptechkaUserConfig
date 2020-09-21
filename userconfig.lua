76ddeb6938b3cb0f5ab4b61a6419ad6915d54efe
d87 <d87d87@gmail.com>
2020-09-21 22:08:36 +0700

    UserConfig for 9.0

--- userconfig.lua
+++ userconfig.lua
@@ -4,43 +4,46 @@ local isHealer = (playerClass == "PRIEST" or playerClass == "PALADIN" or playerC
 local A = helpers.AddAura
 local Trace = helpers.AddTrace
 local config = AptechkaDefaultConfig
+local set = helpers.set
 
 -- config.initialConfigPostHookSnippet = [[
 --     UnregisterUnitWatch(self)
 --     RegisterStateDriver(self, 'visibility', '[mod:shift][combat][@target,exists] show; hide')
 -- ]]
 
-config.TargetStatus = { name = "Target", assignto = "border", color = {0.7,0.2,0.5}, priority = 65 }
-config.MouseoverStatus = { name = "Mouseover", assignto = "border", color = {1,0.5,0.8}, priority = 66 }
+config.TargetStatus = { name = "Target", assignto = set("border"), color = {0.7,0.2,0.5}, priority = 65 }
+config.MouseoverStatus = { name = "Mouseover", assignto = set("border"), color = {1,0.5,0.8}, priority = 66 }
 
-config.LeaderStatus = { name = "Leader", priority = 59, assignto = "LeaderIndicator", color = {1,.8,.2} }
-config.AssistStatus = { name = "Assist", priority = 59, assignto = "AssistantIndicator", color = {1,.8,.2} }
+config.LeaderStatus = { name = "Leader", priority = 59, assignto = set("LeaderIndicator"), color = {1,.8,.2} }
+config.AssistStatus = { name = "Assist", priority = 59, assignto = set("AssistantIndicator"), color = {1,.8,.2} }
 
 if playerClass == "DRUID" then
-    -- A{ id = 1126,  type = "HELPFUL", assignto = { "bars" }, color = { 235/255 , 145/255, 199/255}, isMissing = true } --Mark of the Wild
-    -- A{ id = 33763, type = "HELPFUL", assignto = { "spell3", }, showDuration = true, isMine = true, color = { 0.5, 1, 0.5}, } --Lifebloom
-    -- A{ id = 102351, type = "HELPFUL", assignto = { "spell2" }, priority = 70, color = {38/255, 221/255, 163/255}, isMine = true } --Cenarion Ward
-    -- A{ id = 774,   type = "HELPFUL", assignto = { "spell1"}, pulse = true, color = { 1, 0.2, 1}, showDuration = true, isMine = true } --Rejuvenation
-    -- A{ id = 8936,  type = "HELPFUL", assignto = { "spell3" }, priority = 82, color = { 198/255, 233/255, 80/255}, showDuration = true, isMine = true } --Regrowth
+    -- A{ id = 1126,  type = "HELPFUL", assignto = set("bars"), color = { 235/255 , 145/255, 199/255}, isMissing = true } --Mark of the Wild
+    -- A{ id = 33763, type = "HELPFUL", assignto = set("spell3")}, showDuration = true, isMine = true, color = { 0.5, 1, 0.5}, } --Lifebloom
+    -- A{ id = 102351, type = "HELPFUL", assignto = set("spell2"), priority = 70, color = {38/255, 221/255, 163/255}, isMine = true } --Cenarion Ward
+    -- A{ id = 774,   type = "HELPFUL", assignto = set("spell1"), pulse = true, color = { 1, 0.2, 1}, showDuration = true, isMine = true } --Rejuvenation
+    -- A{ id = 8936,  type = "HELPFUL", assignto = set("spell3"), priority = 82, color = { 198/255, 233/255, 80/255}, showDuration = true, isMine = true } --Regrowth
 end
 
 config.OfflineStatus = { name = "OFFLINE", assignto = { "text2","text3","health","power" }, color = {.15,.15,.15}, textcolor = {1,1,0}, text = "OFFLINE",  priority = 70}
 config.AwayStatus = { name = "AFK", assignto = { "text2","text3" }, color = {.15,.15,.15}, textcolor = {0,128,0}, text = "AFK",  priority = 60}
 
-Aptechka.PostFrameCreate = function(self)
+Aptechka:RegisterWidget("LeaderIndicator", function(self)
     self.LeaderIndicator = self.health:CreateTexture("$parentLeaderIcon", "OVERLAY", nil, 7)
     self.LeaderIndicator:SetSize(12, 12)
     self.LeaderIndicator:SetPoint("LEFT", self.health, "TOPLEFT", 1, 3)
     self.LeaderIndicator:SetTexture([[Interface\GroupFrame\UI-Group-LeaderIcon]])
-    self.LeaderIndicator.SetJob = function() end
     self.LeaderIndicator:Hide()
-
+end)
+Aptechka:RegisterWidget("AssistantIndicator", function(self)
     self.AssistantIndicator = self.health:CreateTexture("$parentAssistIcon", "OVERLAY", nil, 7)
     self.AssistantIndicator:SetSize(12, 12)
     self.AssistantIndicator:SetPoint("LEFT", self.health, "TOPLEFT", 1, 3)
     self.AssistantIndicator:SetTexture([[Interface\GroupFrame\UI-Group-AssistantIcon]])
-    self.AssistantIndicator.SetJob = function() end
     self.AssistantIndicator:Hide()
+end)
+
+Aptechka.PostFrameCreate = function(self)
 end
 
 Aptechka.PostFrameUpdate = function(self)
